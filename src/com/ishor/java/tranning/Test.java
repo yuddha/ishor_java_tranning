5ffc016c93aff2d41a8e10ffa330c8d6a00c311a
user <user@acem-001-046>
2015-07-18 14:55:01 +0545

    printing statement changed

--- src/com/ishor/java/tranning/Test.java
+++ src/com/ishor/java/tranning/Test.java
@@ -4,6 +4,6 @@ package com.ishor.java.tranning;
 public class Test {
 
 	public static void main (String[] args){
-		System.out.println("Hello World!"); // Display the string.
+		System.out.println("Hello World madan!"); // Display the string.
    	 }
 }
